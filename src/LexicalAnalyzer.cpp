e5ffcf613ce0ac5e447f911b770e4ce13b0a1fe3
khamakm <kham.04@bk.ru>
2025-12-05 02:03:58 +0500

    Add TODO list Add comments Feature: merge maps keywords and operators (operations) to lixems Feature: auto detect type of lixem

--- src/LexicalAnalyzer.cpp
+++ src/LexicalAnalyzer.cpp
@@ -1,44 +1,48 @@
 /* 
-    TODO: Дописать обработку типов
-    TODO: изменить распозноваие типов на уровне самих возможных типов
-    TODO: Автоматически определть тип ликсемы. сейчас вручную пишется
+    TODO: добавить типы ликсем
+    TODO: добавить вывод в виде выровненной таблицы ликсем (ТЛ)
+    TODO: добавить таблицу идентификаторов (ТИ) (пока можно без неё)
+    TODO: дописать комметарии и перевести на русский
+    TODO: пофиксить: сначала сдвигается cur char на след со сдвигом позиции, а потом только возврат Token. Поднять до getAllToken nextChar
+    TODO: обработка 8 и 16 (можно пока без этого)
+    TODO: раскидать в приват всё, что надо
 */
 
 #include "../inc/LexicalAnalyzer.hpp"
 
-// Input source_code and first char
-LexicalAnalyzer::LexicalAnalyzer(string sc) : source_code(sc), line(0), column(0) {
+// Инициализация лексического анализатора: загрузка исходного кода и первого символа
+LexicalAnalyzer::LexicalAnalyzer(string sc) : source_code(sc), line(1), column(1), position(0) {
             if (source_code.empty()) current_char = '\0';
             else current_char = source_code[0];
         }
 
-// move to next char
+// Смещение на следующий символ
 void LexicalAnalyzer::nextChar(int count) {
-    for (int i=1; i<count+1; i++) {
+    for (int i=1; i<=count; i++) {
         position++;
         column++;
         if (position >= source_code.length()) current_char = '\0';
         else {
             current_char = source_code[position];
             if (current_char == '\n') {
-                column == 1;
+                column = 0;
                 line++;
             }
         }
     }
-    
+    cout << "Curr chr: " << current_char << endl;
 }
 
-// skip comment
+// Пропуск комментария
 void LexicalAnalyzer::skipComment() {
-    nextChar(1);
+    nextChar(1); // Пропуск шапки комментария
     while (current_char != COMMENT_CLOSE) {
-        nextChar(1);
+        nextChar(1); // Пропуск тела комментария
     }
-    nextChar(1);
+    nextChar(1); // Пропуск подвала коментария
 }
 
-// return token const num
+// Возврат константных типов ликсемы
 Token LexicalAnalyzer::readNumber() {
     string num = "";
     bool is_float = false;
@@ -58,67 +62,59 @@ Token LexicalAnalyzer::readNumber() {
     else return Token(INT_CONSTANT, num, line, column);
 }
 
-// Возврат токена типа ключевое слово или идентификатор
+// Возврат типа ликсемы 
 Token LexicalAnalyzer::readKeyword() {
     string word = "";
     
     // Пока текущий символ не терминальный или не оператор сбор слова
-    while (terminals.find(current_char) == terminals.end()
-            && operations.find(to_string(current_char)) == operations.end()) {
-        auto test1 = terminals.find(current_char);
-        auto test2 = terminals.end();
+    while (terminals.find(current_char) == terminals.end()) {
         word += current_char;
         nextChar(1);
     }
 
-    // Если слово есть в списке ключевых слов, то ключевое слово, иначе - идентификатор
-    if (keywords.find(word) != keywords.end()) 
-        return Token(KEYWORD, word, line, column);
-    else if (operations.find(word) != operations.end()) 
-        return Token(OPERATOR, word, line, column);
+    auto lixem = lixems.find(word);
+    // Если слово есть в списке ликсем, то возврат ликсемы
+    if (lixem != lixems.end()) {
+        if (lixem->first == END_PROGRAM)
+            current_char = END_PROGRAM_CHAR;    
+        return Token(lixem->second, word, line, column);
+    }
     else return Token(IDENTIFIER, word, line, column);
 }
 
-// Token LexicalAnalyzer::readTreminal() {
-//     if ()
-// }
-
 Token LexicalAnalyzer::getToken() {
+    // Пропуск пробелов и комментариев, обработка терминальных символов
+    string word = "";
     while (current_char != END_PROGRAM_CHAR) {
         if (current_char == ' ') nextChar(1);
         else if (current_char == COMMENT_OPEN) skipComment();
-        else break;
+        else {
+            auto terminal_token = terminals.find(current_char);
+            if (terminal_token != terminals.end()) {
+                word = current_char;
+                nextChar(1);
+                if (terminal_token->second != SERVICE) 
+                    return Token(terminal_token->second, word, line, column);
+            }
+            else break;
+        }
     }
-    // Если разделитель, то чтение делиметра
-    if (terminals.find(current_char) != terminals.end()) {
-        return readTreminal();
-    }
+    word = current_char;
     // Иначе Если терминальный символ, то возврат
-    else if (current_char == END_PROGRAM_CHAR) {
-        //!
-        return Token(KEYWORD, to_string(current_char), line, column);
-    }
+    if (current_char == END_PROGRAM_CHAR) 
+        return Token(KEYWORD, word, line, column);
     // Иначе Если число, что чтение числа
-    else if (isdigit(current_char)) {
-        return readNumber();
-    }
+    else if (isdigit(current_char)) return readNumber();
     // Иначе Если ключевое слово или идентификатор
-    else if (isalpha(current_char)) {
-        return readKeyword();
-    }
-    // Иначе Если оператор, то чтение оператора
-    else if (operations.find(to_string(current_char)) != operations.end()) {
-        // return readOperator();
-        nextChar(1);
-    }
-    
-    
+    else if (isalpha(current_char)) return readKeyword();
     // Иначе неизвествный символ
-    nextChar(1);
-    return Token(ERROR, to_string(current_char), line, column);
+    else {
+        nextChar(1);
+        return Token(ERROR, word, line, column);
+    }
 }
 
-// Output vector of tokens from source_code
+// Сбор вектора токенов по исходному коду
 vector<Token> LexicalAnalyzer::getAllToken() {
     vector<Token> table_id;
     Token token;
@@ -126,11 +122,11 @@ vector<Token> LexicalAnalyzer::getAllToken() {
     do {
         token = getToken();
         table_id.push_back(token);
-    } while (current_char != '\0'); //token.type != END_OF_FILE
+    } while (current_char != END_PROGRAM_CHAR); 
     return table_id;
 }
 
-// TODO: допилить вывод, вынести в подфункцию мю
+// Вывод в консоль таблицы ликсем
 void LexicalAnalyzer::printAllToken(vector<Token> tokens) {
     cout << tokens.size() << endl;
     cout << "| " << "Ликсема" << " | " << "Тип ликсемы" << " | " << "Строка" << " | " << "Столбец" << " |" << endl;
