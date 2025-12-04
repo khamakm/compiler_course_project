d15374b4493c5a7abad5fe4e5e50fabe44fc2960
khamakm <kham.04@bk.ru>
2025-12-04 21:56:18 +0500

    Add func to processing comments, int and float nums, keywords, operators, identificators Add func to print tokens Add define lixems Add func to get string name of type token Changes to be committed: 	modified:   inc/LexicalAnalyzer.hpp 	modified:   src/LexicalAnalyzer.cpp

--- inc/LexicalAnalyzer.hpp
+++ inc/LexicalAnalyzer.hpp
@@ -1,28 +1,85 @@
 #pragma once
 
+#include <iostream>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
 using namespace std;
+// operations of group attitude
+#define NOTEQUAL        "NE"
+#define EQUAL           "EQ"
+#define LESS_THAN       "LT"
+#define LESS_EQUAL      "LE"
+#define GREATER_THAN    "GT"
+#define GREATER_EQUAL   "GE"
+// operations of group addition
+#define PLUS            "plus"
+#define MINUS           "mun"
+#define OR              "or"
+// operations of group multiplication
+#define MULT            "mult"
+#define DIV             "div"
+#define AND             "and"
+// unary operations
+#define NOT             "~"
+// assignment operator
+#define ASSIGNMENT       "ass"
+// types
+#define INT             "integer"
+#define FLOAT           "real"
+#define BOOL            "boolean"
+// comment
+#define COMMENT_OPEN    '('
+#define COMMENT_CLOSE   ')'
+// ключевое слова конец программы
+#define END_PROGRAM     "end"
+#define END_PROGRAM_CHAR '\0'
+// булевая константа
+#define TRUE "true"
+#define FALSE "false"
+// оператор цикла
+#define FOR "for"
+#define FOR_TO "to"
+#define FOR_DO "do"
+#define WHILE "while"
+#define WHILE_DO "do"
+// оператор ввода
 
 // Types of lixem
 enum TokenType {
     KEYWORD,
     IDENTIFIER,
-    CONSTANT,
+    INT_CONSTANT,
+    FLOAT_CONSTANT,
+    BOOL_CONSTANT,
     OPERATOR,
-    DELIMITER,
+    TERMINAL,
     END_OF_FILE,
-    ERROR
+    ERROR,
 };
 
 // Struct for keep lixems
 struct Token {
     TokenType type;
-    string value;
-    int line;
+    string value = "";
     int column;
+    int line;
+    
+    Token() {}
+    Token(TokenType t, string v, int l, int c)
+        : type(t), value(v), line(l), column(c) {}
+    string getTypeString() {
+        if (type == 0) return "Ключевое слово";
+        else if (type == 1) return "Идентификатор";
+        else if (type == 2) return "Целочисленная константа";
+        else if (type == 3) return "Константа с плавающей точкой";
+        else if (type == 4) return "Булевая константа";
+        else if (type == 5) return "Оператор";
+        else if (type == 6) return "Терминальный символ";
+        else if (type == 7) return "Ключевое слово";
+        else if (type == 8) return "Ошибка";
+    }
 };
 
 class LexicalAnalyzer {
@@ -31,15 +88,16 @@ class LexicalAnalyzer {
         char current_char;
         int line;
         int column;
-        int position; // position in list source_code
+        int position; // position in source_code
 
         // Table of keywords
         unordered_map<string, TokenType> keywords = {
-            {"int", KEYWORD},
-            {"float", KEYWORD},
-            {"bool", KEYWORD},
-            {"begin", KEYWORD},
-            {"end", KEYWORD},
+            {INT, KEYWORD},
+            {FLOAT, KEYWORD},
+            {BOOL, KEYWORD},
+            // {"begin", KEYWORD},
+            // {"end", KEYWORD},
+            {END_PROGRAM, KEYWORD},
             {"if", KEYWORD},
             {"then", KEYWORD},
             {"else", KEYWORD},
@@ -51,52 +109,50 @@ class LexicalAnalyzer {
             {"while", KEYWORD},
             {"readln", KEYWORD},
             {"writeln", KEYWORD},
-            {"true", CONSTANT},
-            {"false", CONSTANT}
+            {TRUE, BOOL_CONSTANT},
+            {FALSE, BOOL_CONSTANT},
+            {"dim", KEYWORD}
         };
 
         // Таблица операторов
-        unordered_map<string, TokenType> operators = {
-            {"!=", OPERATOR},
-            {"==", OPERATOR},
-            {"<", OPERATOR},
-            {"<=", OPERATOR},
-            {">", OPERATOR},
-            {">=", OPERATOR},
-            {"+", OPERATOR},
-            {"-", OPERATOR},
-            {"||", OPERATOR},
-            {"*", OPERATOR},
-            {"/", OPERATOR},
-            {"&&", OPERATOR},
-            {"!", OPERATOR},
-            {":=", OPERATOR}
+        unordered_map<string, TokenType> operations = {
+            {NOTEQUAL, OPERATOR},
+            {EQUAL, OPERATOR},
+            {LESS_THAN, OPERATOR},
+            {LESS_EQUAL, OPERATOR},
+            {GREATER_THAN, OPERATOR},
+            {GREATER_EQUAL, OPERATOR},
+            {PLUS, OPERATOR},
+            {MINUS, OPERATOR},
+            {OR, OPERATOR},
+            {MULT, OPERATOR},
+            {DIV, OPERATOR},
+            {AND, OPERATOR},
+            {NOT, OPERATOR},
+            {ASSIGNMENT, OPERATOR}
         };
         
         // Table of delititers
-        unordered_map<char, TokenType> delimiters = {
-            {';', DELIMITER},
-            {',', DELIMITER},
-            {'(', DELIMITER},
-            {')', DELIMITER}
+        unordered_map<char, TokenType> terminals = {
+            {',', TERMINAL},
+            {'(', TERMINAL},
+            {')', TERMINAL}
+            // {'\n', TERMINAL},
+            // {'\0', TERMINAL}
         };
 
     public:
+        // TODO раскидать в приват всё, что надо
         // Input source_code and first char
         LexicalAnalyzer(string sc);
-        Token readComment();
+        void nextChar(int count);
+        void skipComment();
         Token readNumber();
-        Token readString();
         Token readKeyword();
-        Token readOperator();
-        Token readDelimiter();
-
-
+        Token readTreminal();
 
         Token getToken();
         vector<Token> getAllToken();
-
-
-
+        void printAllToken(vector<Token> tokens);
 
 };
--- src/LexicalAnalyzer.cpp
+++ src/LexicalAnalyzer.cpp
@@ -1,4 +1,10 @@
-#include "LexicalAnalyzer.hpp"
+/* 
+    TODO: Дописать обработку типов
+    TODO: изменить распозноваие типов на уровне самих возможных типов
+    TODO: Автоматически определть тип ликсемы. сейчас вручную пишется
+*/
+
+#include "../inc/LexicalAnalyzer.hpp"
 
 // Input source_code and first char
 LexicalAnalyzer::LexicalAnalyzer(string sc) : source_code(sc), line(0), column(0) {
@@ -6,37 +12,110 @@ LexicalAnalyzer::LexicalAnalyzer(string sc) : source_code(sc), line(0), column(0
             else current_char = source_code[0];
         }
 
-Token LexicalAnalyzer::getToken() {
-    string current_char_string = to_string(current_char);
+// move to next char
+void LexicalAnalyzer::nextChar(int count) {
+    for (int i=1; i<count+1; i++) {
+        position++;
+        column++;
+        if (position >= source_code.length()) current_char = '\0';
+        else {
+            current_char = source_code[position];
+            if (current_char == '\n') {
+                column == 1;
+                line++;
+            }
+        }
+    }
+    
+}
 
-    // Если комментарий, то удалить комментарий
-    if (current_char == '(' && source_code[position+1] == '*') {
-        readComment();
+// skip comment
+void LexicalAnalyzer::skipComment() {
+    nextChar(1);
+    while (current_char != COMMENT_CLOSE) {
+        nextChar(1);
+    }
+    nextChar(1);
+}
+
+// return token const num
+Token LexicalAnalyzer::readNumber() {
+    string num = "";
+    bool is_float = false;
+
+    while (terminals.find(current_char) == terminals.end()) {
+        if (isdigit(current_char)) {
+            if (source_code[position + 1] == '.') {
+                is_float = true;
+            }
+        }
+
+        num += current_char;
+        nextChar(1);
+    }
+
+    if (is_float) return Token(FLOAT_CONSTANT, num, line, column);
+    else return Token(INT_CONSTANT, num, line, column);
+}
+
+// Возврат токена типа ключевое слово или идентификатор
+Token LexicalAnalyzer::readKeyword() {
+    string word = "";
+    
+    // Пока текущий символ не терминальный или не оператор сбор слова
+    while (terminals.find(current_char) == terminals.end()
+            && operations.find(to_string(current_char)) == operations.end()) {
+        auto test1 = terminals.find(current_char);
+        auto test2 = terminals.end();
+        word += current_char;
+        nextChar(1);
+    }
+
+    // Если слово есть в списке ключевых слов, то ключевое слово, иначе - идентификатор
+    if (keywords.find(word) != keywords.end()) 
+        return Token(KEYWORD, word, line, column);
+    else if (operations.find(word) != operations.end()) 
+        return Token(OPERATOR, word, line, column);
+    else return Token(IDENTIFIER, word, line, column);
+}
+
+// Token LexicalAnalyzer::readTreminal() {
+//     if ()
+// }
+
+Token LexicalAnalyzer::getToken() {
+    while (current_char != END_PROGRAM_CHAR) {
+        if (current_char == ' ') nextChar(1);
+        else if (current_char == COMMENT_OPEN) skipComment();
+        else break;
+    }
+    // Если разделитель, то чтение делиметра
+    if (terminals.find(current_char) != terminals.end()) {
+        return readTreminal();
     }
     // Иначе Если терминальный символ, то возврат
-    else if (current_char == '(' && source_code[position+1] == '*') {
-        ; //!
-    }
-    // Иначе Если кавычки, то чтение строки
-    else if (current_char == '\"') {
-        readString();
+    else if (current_char == END_PROGRAM_CHAR) {
+        //!
+        return Token(KEYWORD, to_string(current_char), line, column);
     }
     // Иначе Если число, что чтение числа
     else if (isdigit(current_char)) {
-        readNumber();
+        return readNumber();
     }
-    // Иначе Если ключевое слова
-    else if (keywords.find(current_char_string) != keywords.end()) {
-        readKeyword();
+    // Иначе Если ключевое слово или идентификатор
+    else if (isalpha(current_char)) {
+        return readKeyword();
     }
     // Иначе Если оператор, то чтение оператора
-    else if (operators.find(current_char_string) != operators.end()) {
-        readOperator();
+    else if (operations.find(to_string(current_char)) != operations.end()) {
+        // return readOperator();
+        nextChar(1);
     }
-    // Иначе Если разделитель, то чтение делиметра
-    else if (delimiters.find(current_char) != delimiters.end()) {
-        readOperator();
-    }
+    
+    
+    // Иначе неизвествный символ
+    nextChar(1);
+    return Token(ERROR, to_string(current_char), line, column);
 }
 
 // Output vector of tokens from source_code
@@ -47,5 +126,15 @@ vector<Token> LexicalAnalyzer::getAllToken() {
     do {
         token = getToken();
         table_id.push_back(token);
-    } while (token.type != END_OF_FILE);
+    } while (current_char != '\0'); //token.type != END_OF_FILE
+    return table_id;
+}
+
+// TODO: допилить вывод, вынести в подфункцию мю
+void LexicalAnalyzer::printAllToken(vector<Token> tokens) {
+    cout << tokens.size() << endl;
+    cout << "| " << "Ликсема" << " | " << "Тип ликсемы" << " | " << "Строка" << " | " << "Столбец" << " |" << endl;
+    for (Token token : tokens) {
+        cout << "| " << token.value << " | " << token.getTypeString() << " | " << token.line << " | " << token.column << " |" << endl;
+    }
 }
